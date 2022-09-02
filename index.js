91b8fb81f9b083510453084b602881a1adc4fbd7
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-09-02 11:55:03 +0700

    add

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 3000;
 const router = require("./router.js");
 const bodyParser = require("body-parser");
-
+const path = require("path");
 // parse application/x-www-form-urlencoded
+app.use('/public', express.static(path.join(__dirname, '/public')))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
@@ -12,6 +13,11 @@ app.use(bodyParser.json())
 
 
 app.use("/api/v1/", router);
+
+app.get('/', (req, res) => {
+  const duongdanfile = path.join(__dirname, 'home.html')
+  res.sendfile(duongdanfile)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
